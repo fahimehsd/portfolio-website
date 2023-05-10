5aa15826ae15b202f9cba0a849adcb11857fa577
fahimehsd <fahimehsadeghi3@gmail.com>
2023-05-10 19:48:18 +0330

    home created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "./components/NavBar";
+import Home from "./components/Home";
 
 const App = () => {
   return (
     <div>
       <NavBar />
+      <Home />
     </div>
   );
 };
