fc1c621bb8f6fc6c6d93fb0ae4994e62b42d27b7
fahimehsd <fahimehsadeghi3@gmail.com>
2023-05-11 17:22:32 +0330

    project done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import me from "../assets/KHODAM.jpg";
 import { MdKeyboardArrowRight } from "react-icons/md";
+import { Link } from "react-scroll";
 const Home = () => {
   return (
     <div
@@ -20,12 +21,17 @@ const Home = () => {
           </p>
 
           <div>
-            <button className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
+            <Link
+              to="portfolio"
+              smooth
+              duration={500}
+              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+            >
               Portfolio
               <span className="group-hover:rotate-90 duration-300">
                 <MdKeyboardArrowRight size={25} className="ml-1" />
               </span>
-            </button>
+            </Link>
           </div>
         </div>
         <div>
